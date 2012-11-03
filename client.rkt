a2b5e722200f7721272ca095ed897e5f621f9502
richo <richo@psych0tik.net>
2012-11-03 15:17:17 +1100

    rewrite the bulk of the functionality in racket

---
+++ client.rkt
@@ -0,0 +1,29 @@
+(define wsurl "ws://agent99.example.org:9001")
+(require net/websocket/client)
+(require net/url)
+
+(define (handle line)
+  (print line))
+
+(define (start-client url)
+  (define-values (sock) (ws-connect url))
+  (let mainloop ()
+    (let ((line (ws-recv sock)))
+      (if (eof-object? line)
+          (exit)
+          (handle line)
+      )
+
+    )
+  (mainloop)
+)
+)
+
+(define (main argv)
+    (start-client (string->url wsurl))
+)
+
+(main '())
+
+
+
